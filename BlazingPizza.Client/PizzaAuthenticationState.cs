8918dc41b332b43279a2b15c9b9af12684a17929
Jocoboy <Jocoboy@outlook.com>
2021-12-23 14:49:35 +0800

    new branch: learning branch for source code analysis.

--- BlazingPizza.Client/PizzaAuthenticationState.cs
+++
@@ -1,7 +0,0 @@
-using BlazingPizza;
-using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
-
-public class PizzaAuthenticationState : RemoteAuthenticationState
-{
-    public Order Order { get; set; }
-}
